cf9f0bf6ff485d1cfcd1bd5cb0b50dea6fd4afd3
ScoldingCoder <scold@Xubuntu.myguest.virtualbox.org>
2023-11-19 23:09:21 -0400

    Changes to be committed: 	modified:   TicTacToe/TicTacToe.rb     Added basic introduction and tutorial to Game Class initialize method

--- TicTacToe/TicTacToe.rb
+++ TicTacToe/TicTacToe.rb
@@ -15,7 +15,6 @@ class Player
         self.name = gets.chomp
         puts "Hello #{self.name}"
 
-
     end   
 
 end 
@@ -23,16 +22,37 @@ end
 
 class Game < Player
 
-    def initialize
     
+
+    def initialize 
+        board = [1,2,3,4,5,6,7,8,9]
+        puts "Hello! This is a console edition of Tic-Tac-Toe.
+To play this edition, each player enters a number to place their symbol on the board.
+The number entered corresponds to a position on the board as shown in the sample board below:
+
+"
+        puts  darwBoard(board)
+        puts "If the number is not a valid position or the position has already been played, You will be asked to make another selection
+Good Luck and have fun!"
+         
+    end
+
+    def darwBoard (boardArray)
+
+        puts "#{boardArray [0]} | #{boardArray [1]} | #{boardArray [2]} " 
+        puts "-----------"
+        puts "#{boardArray [3]} | #{boardArray [4]} | #{boardArray [5]} "
+        puts "-----------"
+        puts "#{boardArray [6]} | #{boardArray [7]} | #{boardArray [8]} "
+      
+
     end
 
 end    
 
 
 
-
-playerOne = Player.new( "Player One", " 'X' ")
-playerOne.changeName
-playerTwo = Player.new( "Player Two", " 'O' ")
-playerTwo.changeName
+ticTacToe = Game.new
+#board = Array.new(9)
+#board = [1,2,3,4,5,6,7,8,9]
+#ticTacToe.darwBoard(board)
